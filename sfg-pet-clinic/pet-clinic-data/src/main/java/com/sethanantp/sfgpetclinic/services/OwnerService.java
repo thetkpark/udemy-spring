31fceeb9c192a3d13e8f2ca19ac8470c22fde1e6
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-17 23:06:13 +0700

    Refactor to common crud interface. Close #10

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,7 @@ package com.sethanantp.sfgpetclinic.services;
 
 import com.sethanantp.sfgpetclinic.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner, Long> {
 
-public interface OwnerService {
     Owner findByLastName(String lastname);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/PetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.sethanantp.sfgpetclinic.services;
 
 import com.sethanantp.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
