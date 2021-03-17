87959d8bb34241b23bbc8f54240f87b6c3eb26a0
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-17 14:36:26 +0700

    adding POJOs. Close #2

---
+++ sfg-pet-clinic/src/main/java/com/sethanantp/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.sethanantp.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ sfg-pet-clinic/src/main/java/com/sethanantp/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sethanantp.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
