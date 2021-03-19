fefef201bd8fceac07769c928ec41b89443dd740
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-19 14:53:26 +0700

    Profile and Primary

--- sfg-di/src/main/java/com/sethanantp/sfgdi/config/GreetingServiceConfig.java
+++ sfg-di/src/main/java/com/sethanantp/sfgdi/config/GreetingServiceConfig.java
@@ -1,10 +1,10 @@
 package com.sethanantp.sfgdi.config;
 
-import com.sethanantp.sfgdi.services.ConstructorGreetingService;
-import com.sethanantp.sfgdi.services.PropertyInjectedGreetingService;
-import com.sethanantp.sfgdi.services.SetterInjectedGreetingService;
+import com.sethanantp.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -22,5 +22,23 @@ public class GreetingServiceConfig {
     @Bean
     SetterInjectedGreetingService setterInjectedGreetingService() {
         return new SetterInjectedGreetingService();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    @Profile("EN")
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Bean("i18nService")
+    @Profile({"ES", "default"})
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
     }
 }
--- sfg-di/src/main/java/com/sethanantp/sfgdi/services/PrimaryGreetingService.java
+++ sfg-di/src/main/java/com/sethanantp/sfgdi/services/PrimaryGreetingService.java
@@ -1,10 +1,5 @@
 package com.sethanantp.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
 public class PrimaryGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
