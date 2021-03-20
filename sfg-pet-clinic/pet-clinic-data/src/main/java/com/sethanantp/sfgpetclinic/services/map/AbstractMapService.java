45f26173180ddd1cae85d63ccdf230082186e560
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-20 10:46:55 +0700

    Refectored Map Impl. Closes #22

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/sethanantp/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.sethanantp.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.sethanantp.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(this.map.values());
@@ -16,9 +15,16 @@ public abstract class AbstractMapService<T, ID> {
         return this.map.get(id);
     }
 
-    T save(ID id, T object) {
-        this.map.put(id, object);
-        return object;
+    T save(T object) {
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(this.getNextId());
+            }
+            this.map.put(object.getId(), object);
+            return object;
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
     }
 
     void deleteById(ID id) {
@@ -27,5 +33,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         this.map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try{
+            nextId = Collections.max(this.map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
