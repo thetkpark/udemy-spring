224824674361deeb74805259274b3e5ec14e6140
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-03-30 09:16:25 +0700

    [ADD] Conmmand objects and converter

---
+++ recipe-app/src/main/java/com/sethanantp/recipeapp/converters/CategoryCommandToCategory.java
@@ -0,0 +1,30 @@
+package com.sethanantp.recipeapp.converters;
+
+
+import com.sethanantp.recipeapp.commands.CategoryCommand;
+import com.sethanantp.recipeapp.domain.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
